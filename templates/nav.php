873558c01c20997e3ede32d1d6cba7b0da4dc5a3
Richard Liu <richard1421@msn.com>
2016-09-16 22:05:15 +1000

    Added placeholder image in banner

--- templates/nav.php
+++ templates/nav.php
@@ -1,8 +1,10 @@
-<div id="banner" class="red lighten-1 valign-wrapper">
-    <div class="white-text valign"><a class="white-text" href="index.php">Title</a></div>
-</div>
-<div id="nav" class="red darken-1 right-align">
-    <a href="index.php" class="waves-effect waves-light white-text nav-btn left"><i class="material-icons left">home</i>Home</a>
-    <a href="cart.php" class="waves-effect waves-light white-text nav-btn"><i class="material-icons left">shopping_cart</i>Cart <span id="cart-count" class="blue darken-2">15</span></a>
-    <a href="login.php" class="waves-effect waves-light white-text nav-btn"><i class="material-icons left">account_circle</i>Account</a>
+<div id="banner-container">
+    <div id="banner" class=" lighten-1 valign-wrapper">
+        <div class="white-text valign"><a class="white-text" href="index.php">Title</a></div>
+    </div>
+    <div id="nav" class=" darken-1 right-align">
+        <a href="index.php" class="waves-effect waves-light white-text nav-btn left"><i class="material-icons left">home</i>Home</a>
+        <a href="cart.php" class="waves-effect waves-light white-text nav-btn"><i class="material-icons left">shopping_cart</i>Cart <span id="cart-count" class="blue darken-2">15</span></a>
+        <a href="login.php" class="waves-effect waves-light white-text nav-btn"><i class="material-icons left">account_circle</i>Account</a>
+    </div>
 </div>
