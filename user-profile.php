78c30f20055f35c8929257b1e2c5f508e61e3c33
Richard Liu <richard1421@msn.com>
2016-09-20 20:01:56 +1000

    Fixed page titles

--- user-profile.php
+++ user-profile.php
@@ -9,7 +9,7 @@ require_once("php/global.php");
 <html>
     <head>
         <?php require_once("templates/head.php") ?>
-        <title>Contact | <?php echo $TITLE ?></title>
+        <title>Profile | <?php echo $TITLE ?></title>
     </head>
 
     <body>
