fe605bf02f3f9b7b3c5742169da9f45840d1648a
Richard Liu <richard1421@msn.com>
2016-09-30 13:42:26 +1000

    Edited user profile page

--- user-profile.php
+++ user-profile.php
@@ -28,15 +28,17 @@ if (!isset($_SESSION["user"]))
                 <div class="col s12 m9">
                     <h5>Profile</h5>
                     <p>Name: <?php echo $_SESSION["user"]->name(); ?></p>
-                    <div class="section">
-                        <h5>Contact</h5>
-                        <p>E-mail: <?php echo $_SESSION["user"]->email; ?><br>
-                        <p>Phone: <?php echo $_SESSION["user"]->phone; ?></p>
-                    </div>
+
                     <div class="section">
                         <h5>Address</h5>
                         <?php echo $_SESSION["user"]->formattedAddress(); ?>
                     </div>
+
+                    <div class="section">
+                        <h5>Contact</h5>
+                        <p>E-mail: <?php echo $_SESSION["user"]->email; ?><br>Phone: <?php echo $_SESSION["user"]->phone; ?></p>
+                    </div>
+                    
                     <div class="section">
                         <a class="waves-effect waves-light btn-flat blue white-text">Edit</a>
                     </div>
